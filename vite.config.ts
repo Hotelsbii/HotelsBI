97dd495f1a1d9ac95945fa84129f437bc8dab502
Hotelsbii <hotelsbi@outlook.fr>
2026-01-08 23:45:58 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: './', // <--- AJOUTE CETTE LIGNE ICI
+    server: {
+      port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
